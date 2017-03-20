aa9e210f2fbac12dbb61bd9d62c1d5fede045f3b
roemhildtg <roemhildtg@gmail.com>
2017-03-20 12:20:10 -0500

    switch to amd output

--- steal-svelte-plugin.js
+++ steal-svelte-plugin.js
@@ -2,20 +2,15 @@ import loader from '@loader';
 import { compile } from 'svelte';
 
 export function translate(load) {
-    return new Promise((resolve, reject) => {
-        const result = compile(load.source, { format: 'eval' });
-        resolve(result.code);
-    });
-}
-
-export function instantiate(load) {
-  const module = eval(load.source);
-    return {
-        deps: [],
-        execute: function() {
-            return loader.newModule({
-                default: module
-            });
+    load.metadata = {
+      format: 'amd'
+    };
+    const result = compile(load.source, {
+        format: 'amd',
+        amd: {
+            id: load.name
         }
-    }
+    });
+    console.log(result.code);
+    return result.code;
 }
