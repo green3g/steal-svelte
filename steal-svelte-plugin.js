c9d60ab319786de525eaf30eaea1f5cdc5f7dd27
roemhildtg <roemhildtg@gmail.com>
2017-04-05 13:30:59 -0500

    cleanup

--- steal-svelte-plugin.js
+++ steal-svelte-plugin.js
@@ -1,11 +1,8 @@
 import { compile } from 'svelte';
-import loader from '@loader';
 
 export function translate(load) {
 
     // get the compiled svelte template
     const compiled = compile(load.source);
-    load.source = compiled.code;
-    // return loader.transpile.call(this, load);
-    return load.source;
+    return compiled.code;
 }
