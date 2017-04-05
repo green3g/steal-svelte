1b579169a1ee8154417aeda482a9dc101ecc51bf
roemhildtg <roemhildtg@gmail.com>
2017-04-05 08:19:46 -0500

    eliminate extra code

--- steal-svelte-plugin.js
+++ steal-svelte-plugin.js
@@ -1,10 +1,7 @@
-import loader from '@loader';
+
 import { compile } from 'svelte';
 
 export function translate(load) {
-    load.metadata = {
-        format: 'es6'
-    };
     let result = compile(load.source, {
         format: 'amd',
         amd: {
